e1d11cdffab7c03a6f0566b35f59abcda65424a2
d6dvth19W5uVRe <52400793@edu.campus02.at>
2025-03-26 14:14:36 +0100

    logging part 2

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -7,12 +7,8 @@ public class Main {
         Calculator calculator = new Calculator();
         System.out.println("Subtract: " + calculator.subtract(5.2, 3));
         System.out.println("Multiply: " + calculator.multiply(5.2, 3));
-        System.out.println("Divide: " + calculator.divide(5.2, 3));
+        System.out.println("Divide: " + calculator.divide(5.2, 0));
         System.out.println("Add: " + calculator.add(5.2, 3));
         System.out.println("Thomas Weissl");
-        Logger logger = LogManager.getLogger();
-        logger.info("Info Message");
-        System.out.println();
-        logger.error("Error Message");
     }
 }
