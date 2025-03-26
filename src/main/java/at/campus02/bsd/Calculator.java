7572e35de9db1df34b2051c0e0c74389775862db
d6dvth19W5uVRe <52400793@edu.campus02.at>
2025-03-26 13:19:01 +0100

    Calculator programm

---
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -0,0 +1,19 @@
+package at.campus02.bsd;
+
+public class Calculator {
+    public double add(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public double subtract(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
+
+    public double divide(double number1, double number2) {
+        return number1 / number2;
+    }
+}
