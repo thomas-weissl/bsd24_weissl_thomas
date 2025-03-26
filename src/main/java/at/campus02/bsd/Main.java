9ac670c192b26e14acc8f0c3376454e203d44aaf
d6dvth19W5uVRe <52400793@edu.campus02.at>
2025-03-26 13:46:05 +0100

    logging

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,5 +1,7 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
@@ -8,5 +10,9 @@ public class Main {
         System.out.println("Divide: " + calculator.divide(5.2, 3));
         System.out.println("Add: " + calculator.add(5.2, 3));
         System.out.println("Thomas Weissl");
+        Logger logger = LogManager.getLogger();
+        logger.info("Info Message");
+        System.out.println();
+        logger.error("Error Message");
     }
 }
